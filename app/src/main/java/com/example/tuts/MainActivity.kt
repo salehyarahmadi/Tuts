e652da0ff24427867f7a969804ffed5d7fa7b409
GIG\yarahmadi.saleh <yarahmadi.saleh@golrang.com>
2022-09-30 03:19:59 +0330

    bottom navigation using compose

--- app/src/main/java/com/example/tuts/MainActivity.kt
+++ app/src/main/java/com/example/tuts/MainActivity.kt
@@ -12,9 +12,13 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +30,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tuts.ui.theme.TutsTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,33 +38,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             TutsTheme {
                 Surface(
-                    color = MaterialTheme.colors.background,
-                    modifier = Modifier.padding(all = 16.dp)
+                    color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        SearchBar()
-
-                        Spacer(Modifier.height(16.dp))
-
-                        AlignYourBodyRow()
-
-                        Spacer(Modifier.height(32.dp))
-
-                        FavoriteCollectionsGrid()
-                    }
-
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun SearchBarPreview() {
-    TutsTheme {
-        SearchBar()
     }
 }
 
@@ -86,18 +70,6 @@ fun SearchBar(modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun AlignYourBodyElementPreview() {
-    TutsTheme {
-        AlignYourBodyElement(
-            drawable = R.drawable.ab1_inversions,
-            text = R.string.ab1_inversions
-        )
-    }
-}
-
-
 @Composable
 fun AlignYourBodyElement(
     @DrawableRes drawable: Int,
@@ -118,22 +90,10 @@ fun AlignYourBodyElement(
         )
         Text(
             stringResource(text),
-            style = MaterialTheme.typography.h6,
+            style = MaterialTheme.typography.h3,
             modifier = Modifier.paddingFromBaseline(
                 top = 24.dp, bottom = 8.dp
             )
-        )
-    }
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
-@Composable
-fun FavoriteCollectionCardPreview() {
-    TutsTheme {
-        FavoriteCollectionCard(
-            text = R.string.fc2_nature_meditations,
-            drawable = R.drawable.fc2_nature_meditations,
-            modifier = Modifier.padding(8.dp)
         )
     }
 }
@@ -160,7 +120,7 @@ fun FavoriteCollectionCard(
             )
             Text(
                 text = stringResource(text),
-                style = MaterialTheme.typography.h6,
+                style = MaterialTheme.typography.h3,
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
         }
@@ -197,6 +157,78 @@ fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
                 modifier = Modifier.height(56.dp)
             )
         }
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            stringResource(title).uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+            .padding(vertical = 16.dp)
+    ) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+    }
+}
+
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        modifier = modifier
+    ) {
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_home))
+            },
+            selected = true,
+            onClick = {}
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null,
+                )
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_profile))
+            },
+            selected = false,
+            onClick = {}
+        )
     }
 }
 
