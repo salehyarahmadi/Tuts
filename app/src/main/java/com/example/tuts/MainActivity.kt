c89447ae77ad315b139ac66808fb14d6f768d3c6
GIG\yarahmadi.saleh <yarahmadi.saleh@golrang.com>
2022-09-30 02:41:46 +0330

    favorite collections grid using compose

--- app/src/main/java/com/example/tuts/MainActivity.kt
+++ app/src/main/java/com/example/tuts/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -40,12 +43,9 @@ class MainActivity : ComponentActivity() {
 
                         AlignYourBodyRow()
 
-                        Spacer(Modifier.height(16.dp))
+                        Spacer(Modifier.height(32.dp))
 
-                        FavoriteCollectionCard(
-                            drawable = R.drawable.fc2_nature_meditations,
-                            text = R.string.fc2_nature_meditations
-                        )
+                        FavoriteCollectionsGrid()
                     }
 
                 }
@@ -53,11 +53,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-private data class DrawableStringPair(
-    @DrawableRes val drawable: Int,
-    @StringRes val text: Int
-)
 
 
 @Preview(showBackground = true)
@@ -186,6 +181,25 @@ fun AlignYourBodyRow(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun FavoriteCollectionsGrid(modifier: Modifier = Modifier) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(
+                drawable = item.drawable,
+                text = item.text,
+                modifier = Modifier.height(56.dp)
+            )
+        }
+    }
+}
+
 private val alignYourBodyData = listOf(
     R.drawable.ab1_inversions to R.string.ab1_inversions,
     R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
@@ -194,3 +208,17 @@ private val alignYourBodyData = listOf(
     R.drawable.ab5_hiit to R.string.ab5_hiit,
     R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
 ).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
